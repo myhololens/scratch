2cd5185249aca27a63d9fbbdd017086c53966d3d
free <99874405@qq.com>
2019-03-31 15:43:21 +0800

    x

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -270,21 +270,7 @@ class MenuBar extends React.Component {
     }
     handlePushToCloud (projectFilename, projectFilepromise) {
         return () => {
-            projectFilepromise.then(sb3 => {
-                let form = new FormData()
-                    form.append('name', projectFilename)
-                    form.append('sb3', sb3)
-
-                if (qs.search['id']) {
-                    form.append('id', qs.search['id'])
-                }
-
-                fetch({ url: '/api/project/save', body: form, method: 'FORM' }).then(res => {
-                    if (res) {
-                        alert('保存成功')
-                    }
-                })
-            })
+            debugger
         }
     }
     handleLanguageMouseUp (e) {
